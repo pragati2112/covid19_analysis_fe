2f2cf37f50b1f98a6a28b9da6f29c46bc2ae5340
pragati <pragati@ipropal.com>
2020-03-24 16:57:23 +0530

    first commit and echarts example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="main" style="width:1000px;height:400px">
+
   </div>
 </template>
 
 <script>
+
+import axios from "axios";
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+        x_axis: [],
+        y_axis: []
+    }
+  },
+
+  mounted(){
+    this.axiosInstance = axios.create({
+      timeout:50000,
+      headers:{
+        'Content-Type': 'application/json',
+      }
+    });
+
+    this.fetchConfirmedCases();
+    this.drawChart();
+  },
+
+
+  methods:{
+    fetchConfirmedCases(){
+      this.axiosInstance.get("/cases/confirmed")
+              .then(response=>{
+                 var str=response.data;
+                 if (str.includes("\"")) {
+                    var valid_str = JSON.parse(str);
+                 }
+                 console.log(valid_str) ;
+                 this.$data.x_axis = valid_str.json_xax;
+                 this.$data.y_axis = valid_str.json_yax;
+
+              }).catch(error=> {
+                  console.log(error.response.data);
+      });
+
+    },
+      drawChart(){
+          // eslint-disable-next-line no-undef
+          var myChart = echarts.init(document.getElementById('main'));
+          myChart.setOption({
+              title: {
+                  text: 'ECharts entry example'
+              },
+              tooltip: {},
+              xAxis: {
+                  data: ['shirt', 'cardign', 'chiffon shirt', 'pants', 'heels', 'socks']
+              },
+              yAxis: {},
+              series: [{
+                  name: 'sales',
+                  type: 'bar',
+                  data: [5, 20, 36, 10, 10, 20]
+              }]
+          });
+      },
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
